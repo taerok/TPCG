a3a958b173a72378da7887f9aaa7eb12481b5f64
taerok <trpark@tpcg.co.kr>
2021-01-13 14:55:42 +0900

    update: fix url

--- vue/src/utils/http-common.js
+++ vue/src/utils/http-common.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'http://10.178.0.4:8080',
+    baseURL: 'http://34.64.87.69:8080',
 })
